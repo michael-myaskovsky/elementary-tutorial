1f35428a55bae99cfc25cbe8a6315d61210ed5ad
Michael Myaskovsky <michael@elementary-data.com>
2025-04-24 12:55:17 +0000

    Optimize stg_orders model

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,26 +1,15 @@
--- depends_on: {{ ref('orders_validation') }}
-
-{% if elementary.get_config_var('anomalies') %}
-    with source as (
-        select * from {{ ref('orders_validation') }}
-    ),
-
-{% else %}
-    with source as (
-        select * from {{ ref('orders_training') }}
-    ),
-{% endif %}
+with source as (
+    select * from {{ source('elementary_tutorial', 'orders_training') }}
+),
 
 renamed as (
-
     select
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
-
+        status,
+        amount
     from source
-
 )
 
 select * from renamed
