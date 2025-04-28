7a3c087bb99d5146460d31fabfbd03a5950ea925
Michael Myaskovsky <michael@elementary-data.com>
2025-04-28 14:06:05 +0000

    Optimize orders model for improved performance

--- models/orders.sql
+++ models/orders.sql
@@ -1,56 +1,20 @@
-{% set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] %}
-
-with orders as (
-
-    select * from {{ ref('stg_orders') }}
-
-),
-
-payments as (
-
-    select * from {{ ref('stg_payments') }}
-
-),
-
-order_payments as (
-
+with payments as (
     select
         order_id,
-
-        {% for payment_method in payment_methods -%}
-        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount,
-        {% endfor -%}
-
-        sum(amount) as total_amount
-
-    from payments
-
-    group by order_id
-
+        sum(case when status = 'success' then amount end) as amount
+    from {{ ref('stg_payments') }}
+    group by 1
 ),
 
 final as (
-
     select
         orders.order_id,
         orders.customer_id,
         orders.order_date,
         orders.status,
-
-        {% for payment_method in payment_methods -%}
-
-        order_payments.{{ payment_method }}_amount,
-
-        {% endfor -%}
-
-        order_payments.total_amount as amount
-
-    from orders
-
-
-    left join order_payments
-        on orders.order_id = order_payments.order_id
-
+        coalesce(payments.amount, 0) as amount
+    from {{ ref('stg_orders') }} as orders
+    left join payments on orders.order_id = payments.order_id
 )
 
 select * from final
