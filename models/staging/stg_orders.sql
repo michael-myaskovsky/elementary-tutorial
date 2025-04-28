f1e5e836f4c30b9beada1282adf32784ccbd5b74
Michael Myaskovsky <michael@elementary-data.com>
2025-04-28 09:52:36 +0000

    Optimize stg_orders model by explicitly selecting columns

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,26 +1,19 @@
--- depends_on: {{ ref('orders_validation') }}
-
-{% if elementary.get_config_var('anomalies') %}
-    with source as (
-        select * from {{ ref('orders_validation') }}
-    ),
-
-{% else %}
-    with source as (
-        select * from {{ ref('orders_training') }}
-    ),
-{% endif %}
+with source as (
+    select 
+        id,
+        user_id,
+        order_date,
+        status
+    from {{ source('public', 'orders_training') }}
+),
 
 renamed as (
-
     select
         id as order_id,
         user_id as customer_id,
         order_date,
         status
-
     from source
-
 )
 
 select * from renamed
